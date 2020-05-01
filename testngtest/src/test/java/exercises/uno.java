15adf075001afc1f83a072b708da30e86df632b4
DonChaquetas <misterwhysucks@gmail.com>
2020-05-01 17:28:05 -0600

    Adding MercadoLibre excercise

---
+++ testngtest/src/test/java/exercises/uno.java
@@ -0,0 +1,11 @@
+package exercises;
+
+import org.openqa.selenium.WebDriver;
+import utils.WebDriverFactory;
+
+public class uno {
+
+    public static void main(String[] args){
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+    }
+}
