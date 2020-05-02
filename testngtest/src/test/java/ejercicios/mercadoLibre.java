f60a3845de48f336b10186fa4dbd9b8da035a762
DonChaquetas <misterwhysucks@gmail.com>
2020-05-02 08:10:30 -0600

    Adding Fer Ejercicios

---
+++ testngtest/src/test/java/ejercicios/mercadoLibre.java
@@ -0,0 +1,25 @@
+package ejercicios;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import utils.WebDriverFactory;
+import java.util.List;
+public class mercadoLibre {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        WebDriverWait wait = new WebDriverWait(driver,10);
+        driver.get("https://www.mercadolibre.com.mx/");
+        driver.manage().window().maximize();
+        WebElement mlSearchBox = driver.findElement(By.name("as_word"));
+        mlSearchBox.sendKeys("Selenium");
+        WebElement mlSearchButton = driver.findElement(By.className("nav-search-btn"));
+        mlSearchButton.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("main-title")));
+        List<WebElement> listOfProducts = driver.findElements(By.className("main-title"));
+        System.out.println(listOfProducts.get(0).getText());
+        listOfProducts.get(0).click();
+        driver.quit();
+    }
+}
