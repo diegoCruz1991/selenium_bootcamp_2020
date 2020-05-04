8fa0dc9b55ce5b15604703a50cdaa80d7f7b2140
deolivaa082 <63917195+deolivaa082@users.noreply.github.com>
2020-05-04 00:45:14 -0500

    commit

---
+++ testngtest/src/test/java/ejercicios/LocatorsExercise2.java
@@ -0,0 +1,30 @@
+package ejercicios;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import utils.WebDriverFactory;
+
+import java.util.concurrent.TimeUnit;
+
+public class LocatorsExercise2 {
+    public static void main (String[] args){
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        //driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("MY ID")));
+        // Abre Browser
+        driver.get("http://www.google.com.mx");
+        assert driver.getCurrentUrl().equals("http://www.google.com.mx"): "Pagina Correcta";
+        WebElement searchElem = driver.findElement(By.id("JustAnInvalidID"));
+        searchElem.click();
+        driver.close();
+
+
+
+
+    }
+
+}
