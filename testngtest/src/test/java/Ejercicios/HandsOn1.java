7ecf22eebcff1ffa5f91d77130947907c202f13e
Daniel Albavera <dalbavera@hotmail.com>
2020-05-08 19:23:49 -0500

    Small fixes on HandsOn1 and HandsOn2

--- testngtest/src/test/java/Ejercicios/HandsOn1.java
+++ testngtest/src/test/java/Ejercicios/HandsOn1.java
@@ -32,58 +32,72 @@ public class HandsOn1 {
         WebElement button_three_stars;
         WebElement button_four_stars;
 
-        String css_product_base;
-        String css_price_row;
-        WebElement price;
+        String xpath_product_base;
+        String xpath_price_row;
+        WebElement price = null;
         List<Float> prices_float = new ArrayList<Float>();
 
         // ***** TEST STEPS *****
 
         //Search Nintendo Switch
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
         search_bar = driver.findElement(By.cssSelector("*[id='twotabsearchtextbox']"));
         search_bar.sendKeys("nintendo switch");
         search_bar.sendKeys(Keys.ENTER);
 
-        //Select Checkbox Prime
-        //Implicit Wait
-        Thread.sleep(3000);
-        checkbox_prime = driver.findElement(By.cssSelector("i[class='a-icon a-icon-checkbox']"));
-        checkbox_prime.click();
-        System.out.println("presioné prime");
-
         //Select Products with 3 or more stars
         //Implicit Wait
-        Thread.sleep(3000);
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
         button_three_stars = driver.findElement(By.cssSelector("i[class='a-icon a-icon-star-medium a-star-medium-3"));
         button_three_stars.click();
         System.out.println("presioné 3 estrellas");
 
         //Select Products with 4 or more stars
         //Implicit Wait
-        Thread.sleep(3000);
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
         button_four_stars = driver.findElement(By.cssSelector("i[class='a-icon a-icon-star-medium a-star-medium-4'"));
         button_four_stars.click();
         System.out.println("presioné 4 estrellas");
 
+        //Select Checkbox Prime
+        //Implicit Wait
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        checkbox_prime = driver.findElement(By.cssSelector("i[class='a-icon a-icon-checkbox']"));
+        checkbox_prime.click();
+        System.out.println("presioné prime");
+
         //Wait 5 seconds
-        Thread.sleep(10000);
+        Thread.sleep(5000);
         //This xpath retrieve a List with all divs that contains the search results products
-        List<WebElement> div_list = driver.findElements(By.cssSelector("div[class='s-result-list s-search-results sg-row'] > div > div"));
+        List<WebElement> div_list = driver.findElements(By.xpath("//div[contains(@class,'s-result-list s-search-results')]/div/div"));
         //the size of the list is the total amount of products
         System.out.println("\nTotal products = "+div_list.size()+"\n");
 
-        //base xpath
-        css_product_base = "div[data-index='";
-        //xpath to locate the price row
-        css_price_row = "'] > * > * > * > * > * > * > * > * > * > * > * > * > * > * > * > span";
+        //xpath for all elements grid $x("//div[contains(@class,'s-search-results')][2]/div/div")
+        //xpath for prices grid  $x("//div[@data-index='2']//span[@class='a-offscreen']")
+
+        //xpath for all elements list $x("//div[contains(@class,'s-result-list')][2]/div/div/*")
+        //xpath for price list $x("//div[@data-index='2']//span[@class='a-offscreen']")
+
+        //base css
+        xpath_product_base = "//div[@data-index='";
+        //css to locate the price row
+        xpath_price_row = "']//span[@class='a-offscreen']";
 
         //This loop iterates the product list
         for(int i = 0; i < div_list.size() - 1; i++ ) {
-            //price = css_base + i + css_row
-            price = driver.findElement(By.cssSelector(css_product_base + i + css_price_row));
+            //Try because some products don't have price
+            try{
+                //price = css_base + i + css_row
+                price = driver.findElement(By.xpath(xpath_product_base + i + xpath_price_row));
+            }
+            catch (Exception e){
+                //this just will ignore the product without price
+                price = null;
+            }
             //List of Strings to sort in order to get the cheaper product
             //Validate if the string is not empty
-            if(!price.getAttribute("textContent").isEmpty()) {
+            if(price != null && !price.getAttribute("textContent").isEmpty()) {
                 //print price
                 System.out.println(price.getAttribute("textContent"));
                 //save the price into a list of floats using replace to delete useless characters
@@ -92,7 +106,7 @@ public class HandsOn1 {
         }
 
         //Validate if the list of floats is not empty
-        if(prices_float.isEmpty()==false) {
+        if(!prices_float.isEmpty()) {
             //Use the Collection.sort(List) to re-order the prices
             Collections.sort(prices_float);
             System.out.println("\nSorted List:\n");
@@ -109,6 +123,6 @@ public class HandsOn1 {
         }
 
         //Close current Window
-        //driver.close();
+        driver.close();
     }
 }
--- testngtest/src/test/java/Ejercicios/HandsOn2.java
+++ testngtest/src/test/java/Ejercicios/HandsOn2.java
@@ -4,12 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.Select;
 import utils.WebDriverFactory;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -33,24 +29,21 @@ public class HandsOn2 {
         // ***** WEB ELEMENTS *****
         WebElement dropdown;
         WebElement places;
-        Actions actions;
         WebElement search_bar;
         WebElement element;
 
         // ***** TEST STEPS *****
         //Implicit Wait 10 seconds
         driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
-        //Get Dropdown
-        dropdown = driver.findElement(By.cssSelector("li[class='navtab moretab menutab'] > div[class='dropdown']"));
-        //create Actions Object
-        actions = new Actions(driver);
-        //move the mouse to the dropdown to make visible the inside elements
-        actions.moveToElement(dropdown).perform();
 
-        //Get places
-        places = driver.findElement(By.xpath("//a[contains(text(),'Places')]"));
-        //move mouse to places to click it while is visible
-        actions.moveToElement(places).perform();
+        //Get dropdown
+        dropdown = driver.findElement(By.xpath("//*[@id='header-more-dropdown-toggle']/i[@class='fa fa-angle-down']"));
+        //Double click to keep the inside elements active
+        dropdown.click();
+        dropdown.click();
+
+        //Get Places
+        places = driver.findElement(By.xpath("//a[@href='/places']"));
         places.click();
 
         //Get search bar
@@ -73,11 +66,12 @@ public class HandsOn2 {
             System.out.println("Type: "+list_names.get(i).getText());
             //increment the index to get the next element
             i++;
-            //try catch because if the specias has no examples the driver can trigger an exception
+            //try catch because if the species has no examples the driver can trigger an exception
             try {
                 element = driver.findElement(By.cssSelector("div[class='guide_taxa']>*>*>*>*>*>*"));
                 System.out.println("Example: " + element.getText() + "\n");
-            }catch (Exception e){
+            }
+            catch (Exception e){
                 System.out.println("No example for this category\n");
             }
         }
