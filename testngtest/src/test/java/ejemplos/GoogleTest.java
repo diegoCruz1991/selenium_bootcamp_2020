9ad27b1df861b3bab619c1c93d2c293220b19b7f
Diego Cruz <diego1991.cruz@gmail.com>
2020-05-21 14:45:35 -0500

    add allure support

--- testngtest/src/test/java/ejemplos/GoogleTest.java
+++ testngtest/src/test/java/ejemplos/GoogleTest.java
@@ -2,19 +2,20 @@ package ejemplos;
 
 import google.pages.GoogleHome;
 import google.pages.GoogleResultsPage;
+import io.qameta.allure.*;
 import org.testng.Assert;
 import org.testng.asserts.SoftAssert;
 import org.testng.annotations.Test;
 
-//@Epic("Regression Tests")
-//@Feature("First Test")
+@Epic("Google Testing")
+@Feature("First Test")
 public class GoogleTest extends BaseTest {
 
     @Test(alwaysRun = true, priority = 0)
-    //@Severity(SeverityLevel.BLOCKER)
-    //@Description("Check if the Page to be tested is the correct one")
-    //@Story("Get to Google Page")
-    //@Step("Go to Google")
+    @Severity(SeverityLevel.BLOCKER)
+    @Description("Check if the Page to be tested is the correct one")
+    @Story("Get to Google Page")
+    @Step("Go to Google")
     public void validateGoogleURL() {
         myDriver.get(GoogleHome.BASE_URL);
 
@@ -22,7 +23,7 @@ public class GoogleTest extends BaseTest {
 
         SoftAssert softAssertion = new SoftAssert();
 
-        softAssertion.assertEquals(myDriver.getCurrentUrl(), GoogleHome.BASE_URL + "s");
+        softAssertion.assertEquals(myDriver.getCurrentUrl(), GoogleHome.BASE_URL);
 
         softAssertion.assertTrue(home.isLoaded(), "Google page is not loaded");
 
@@ -30,10 +31,10 @@ public class GoogleTest extends BaseTest {
     }
 
     @Test(alwaysRun = true, priority = 1)
-    //@Severity(SeverityLevel.BLOCKER)
-    //@Description("Search something in Google")
-    //@Story("Test Google Search")
-    //@Step("Search in Google Selenium")
+    @Severity(SeverityLevel.BLOCKER)
+    @Description("Search something in Google")
+    @Story("Test Google Search")
+    @Step("Search in Google Selenium")
     public void searchSomething() {
         GoogleHome home = new GoogleHome(myDriver);
 
@@ -45,10 +46,10 @@ public class GoogleTest extends BaseTest {
     }
 
     @Test(priority = 2, dependsOnMethods = {"searchSomething"})
-    //@Severity(SeverityLevel.BLOCKER)
-    //@Description("Search something in Google")
-    //@Story("Test Google Search")
-    //@Step("Click in the result")
+    @Severity(SeverityLevel.BLOCKER)
+    @Description("Search something in Google")
+    @Story("Test Google Search")
+    @Step("Click in the result")
     public void clickOnResultByTitle() throws InterruptedException {
         GoogleResultsPage resultsPage = new GoogleResultsPage(myDriver);
 
